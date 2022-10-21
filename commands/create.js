92c70634938c7782de07e7082828219605a5292b
FredFed <thenedbear@hotmail.it>
2022-10-21 22:27:59 +0200

    minor refactoring

--- commands/create.js
+++ commands/create.js
@@ -4,12 +4,6 @@ const sdk = require('algosdk');
 
 const createDid = require('../tools/create_did');
 
-const bytesToHex = function (byteArray) {
-    return Array.from(byteArray, function(byte) {
-        return ('0' + (byte & 0xFF).toString(16)).slice(-2);
-    }).join('')
-}
-
 // implements the "create" command
 const create = function(args, command) {
 
