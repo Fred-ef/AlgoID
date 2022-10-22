ab2a4e71ac2dc6e30360eb768195b4e077f05f5e
FredFed <thenedbear@hotmail.it>
2022-10-23 00:51:00 +0200

    separated helpCommand function updated help files changed "create" command functioning (now it generates an address)

--- algodid.js
+++ algodid.js
@@ -1,41 +1,14 @@
-const fs = require('fs');
-
 const create = require('./commands/create');
+const commandHelp= require('./help/command-help');
 
 
 
 // ########## GLOBAL DATA ##########
 
-const HELP_PATH = './help/';
-const HELP_EXT = '.txt';
 const ALLOWED_COMMANDS = ["create"];
 
+
 // ########## FUN DEF ##########
-
-// helper function that prints a guide on how to use the issued command
-const commandHelp = function(msg, command='general') {
-    // reads the correct helper file and returns
-    // a textual guide on how to use the given command
-
-    if(msg) console.error("\nError: "+msg);     // printig error message
-
-    let filePath = HELP_PATH+command+HELP_EXT;
-    let res = "";
-
-
-    switch (command) {
-        case "create":
-            res = fs.readFileSync(filePath, {encoding:'utf8', flag:'r'});
-            console.error(res);
-            break;
-    
-        default:
-            res = fs.readFileSync(filePath, {encoding:'utf8', flag:'r'});
-            console.error(res);
-            break;
-    }
-}
-
 
 const parseCommand = function() {
     // checking if a command was issued
@@ -58,7 +31,7 @@ const parseCommand = function() {
             commandHelp();
             break;
         default:
-            commandHelp("Command not recognized");
+            commandHelp("Command \""+command+"\" not recognized");
             process.exit(1);
     }
 }
--- commands/create.js
+++ commands/create.js
@@ -1,8 +1,8 @@
 // command-route for did creation
 
 const sdk = require('algosdk');
-
 const createDid = require('../tools/create_did');
+const commandHelp = require('../help/command-help');
 
 // implements the "create" command
 const create = function(args, command) {
@@ -13,15 +13,17 @@ const create = function(args, command) {
         process.exit(1);
     }
 
-    // Algorand address validation
-    const addr = args[0];
-    if(!sdk.isValidAddress(addr)) {
-        commandHelp("Invalid Algorand address", command);
-        process.exit(1);
-    }
+    // getting did's name
+    const name = args[0];
+
+    // generating a new Algorand address
+    const account = sdk.generateAccount();
+    const addr = account.addr;
 
     let did = createDid(addr);
 
+    console.log("DID name: "+name);
+    console.log("DID content:");
     console.log(did);
 }
 
